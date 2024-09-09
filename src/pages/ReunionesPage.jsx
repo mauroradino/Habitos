14150e9a4e011a03ffaa3a41e0862272ecae7bc4
mauroradino <mauroradino2003@gmail.com>
2024-09-09 11:45:14 -0300

    arreglo reuniones page

--- src/pages/ReunionesPage.jsx
+++ src/pages/ReunionesPage.jsx
@@ -3,15 +3,22 @@ import ReunionesJSON from "../assets/reuniones.json"
 const ReunionesPage = () => {
   return (
     <div className="w-full bg-blue-100 pt-32 h-screen p-8 fixed z-10 lg:static lg:z-auto">
-      <ul className="w-full h-full px-4 overflow-auto bg-slate-50 rounded-lg shadow-md">
+      <ul className="w-full max-h-full min-h-max px-4 overflow-auto bg-slate-50 rounded-lg shadow-md">
+      <li className='flex p-2 gap-2 justify-between'>
+            <p className='w-2/12 font-bold text-xs'>Titulo</p>
+            <p className="w-4/12 font-bold text-xs text-center">Equipo</p>
+            <p className="w-2/12 font-bold text-xs text-center">Fecha</p>
+            <p className="w-4/12 font-bold text-xs text-center hidden lg:flex md:flex">Enlace</p>
+          </li>
+          <hr></hr>
         {ReunionesJSON.map((elemento, index) => {
             return(
                 <>
           <li className='flex p-2 gap-2 justify-between' key={index}>
-            <p className='w-3/12'>{elemento.titulo}</p>
-            <p className="w-6/12 text-center">{elemento.equipo}</p>
-            <p className="w-3/12 text-center">{elemento.fecha}</p>
-            <p className="w-3/12 text-center text-blue-500 underline">{elemento.enlace}</p>
+            <p className='w-2/12 text-xs'>{elemento.titulo}</p>
+            <p className="w-4/12 text-xs text-center">{elemento.equipo}</p>
+            <p className="w-2/12 text-xs text-center">{elemento.fecha}</p>
+            <p className="w-4/12 text-xs text-center hidden lg:flex md:flex text-blue-500 underline">{elemento.enlace}</p>
           </li>
           <hr></hr>
             </>
