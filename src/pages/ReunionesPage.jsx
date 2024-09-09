903a81443353908a964bf8ce9fc1e5499cd56adf
mauroradino <mauroradino2003@gmail.com>
2024-09-09 12:17:51 -0300

    scroll reuniones

--- src/pages/ReunionesPage.jsx
+++ src/pages/ReunionesPage.jsx
@@ -2,7 +2,7 @@ import ReunionesJSON from "../assets/reuniones.json"
 
 const ReunionesPage = () => {
   return (
-    <div className="w-full bg-blue-100 pt-32 h-screen p-8 fixed z-10 lg:static lg:z-auto">
+    <div className="w-full bg-blue-100 pt-32 min-h-screen p-8 fixed z-10 lg:static lg:z-auto">
       <ul className="w-full max-h-full min-h-max px-4 overflow-auto bg-slate-50 rounded-lg shadow-md">
       <li className='flex p-2 gap-2 justify-between'>
             <p className='w-2/12 font-bold text-xs'>Titulo</p>
